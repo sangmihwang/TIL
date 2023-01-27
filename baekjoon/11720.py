b374ffc6510078c5e4f780df6ae4495cc49915f2
sangmihwang <01071184366a@gmail.com>
2023-01-27 17:37:42 +0900

    day1

---
+++ baekjoon/11720.py
@@ -0,0 +1,11 @@
+# N = int(input())
+# n = str(input())
+# a = []
+# for i in n:
+#     a.append(int(i))
+# print(sum(a))
+
+
+N = int(input())
+n = map(int, input())
+print(sum(n))
--- baekjoon/2439.py
+++ baekjoon/2439.py
@@ -1,6 +1,6 @@
 N = int(input())
 
 i = 1
-for i in range(1, N+1):
-    print('*' * i)
+for i in range(1, N + 1):
+    print(('*' * i).rjust(N))
 
