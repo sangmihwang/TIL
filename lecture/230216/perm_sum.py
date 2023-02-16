dcde8bb18161cf25c5ff5c7538b5f8da2ae3635e
sangmihwang <01071184366a@gmail.com>
2023-02-16 17:54:37 +0900

    lec

--- lecture/230216/perm_sum.py
+++ lecture/230216/perm_sum.py
@@ -22,20 +22,20 @@
 #     f(0, N)
 
 
-def f(i, k, s):  # s 앞에서 선택한 원소의 합
+def f(i, k, s):                 # s 앞에서 선택한 원소의 합
     global minV
     global cnt
     cnt += 1
-    if i == k:  # 순열 완성
+    if i == k:                  # 순열 완성
         if minV > s:
             minV = s
         return
     elif s >= minV:
         return
     else:
-        for j in range(i, N):  # p[i]의 숫자를 자신부터 오른쪽과 교환해봄
+        for j in range(i, N):   # p[i]의 숫자를 자신부터 오른쪽과 교환해봄
             p[i], p[j] = p[j], p[i]
-            f(i + 1, k, s + arr[i][p[i]])
+            f(i+1, k, s + arr[i][p[i]])
             p[i], p[j] = p[j], p[i]
     return
 
@@ -45,8 +45,8 @@ for tc in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    p = [i for i in range(N)]  # p[i] i행에서 선택한 열
-    minV = 100  # 10이하 자연수 10개 이하
+    p = [i for i in range(N)]           # p[i] i행에서 선택한 열
+    minV = 100                          # 10이하 자연수 10개 이하
     cnt = 0
     f(0, N, 0)
     print(f'#{tc} {minV}')
