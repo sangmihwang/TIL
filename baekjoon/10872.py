343c4fedd4d304149722afa334f907a1560d508b
sangmihwang <01071184366a@gmail.com>
2023-02-22 18:01:18 +0900

    study

--- baekjoon/10872.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-def fac(n):
-    if n == 0:
-        return 1
-    elif n > 0:
-        return n * fac(n-1)
-
-print(fac(N))
--- baekjoon/12865.py
+++
@@ -1,5 +0,0 @@
-# 12865_ 평범한 배낭
-
-N, K = map(int, input().split())
-lst = [tuple(map(int, input().split())) for _ in range(N)]
-
