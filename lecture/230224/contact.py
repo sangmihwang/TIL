0817dad919fc27d8046c4b7cc32093e90fc18c24
sangmihwang <01071184366a@gmail.com>
2023-02-24 18:13:31 +0900

    lec

--- lecture/230224/contact.py
+++ lecture/230224/contact.py
@@ -1,15 +1,30 @@
-for t in range(1, 11):
-    lst, start = map(int, input().split())
-    ppl = list(map(int, input().split()))
-    line = [[0] * (max(ppl)+1) for _ in range(max(ppl))]
+def BFS(s):
+    queue = []              # 큐 생성
+    queue.append(s)         # 시작점 큐에 삽입
+    visited[s] = 0
+    while queue:            # 큐가 비어있지 않은 경우
+        t = queue.pop(0)    # 큐의 첫번째 원소 반환
+        for i in line[t]:
+            if not visited[i]:
+                queue.append(i)                 # 큐에 넣기
+                visited[i] = visited[t] + 1     # n으로부터 1만큼 이동 (이동 횟수)
 
+for t in range(1, 11):
+    lst, s = map(int, input().split())          # 연결쌍 수, 시작점
+    ppl = list(map(int, input().split()))
+    line = [[] for _ in range(101)]             # line[from번호] : [to번호 다]
+    visited = [0] * 101                         # 정점의 개수 100개
     for i in range(lst//2):
-        n = 0
-        if not line[n][ppl[i * 2]]:             # 0이면 그대로 적고
-            line[n][ppl[i * 2]] = [ppl[i * 2 + 1]]
-        else:                                   # 0 아니고 다른 숫자 있으면
-            while line[n][ppl[i * 2]]:
-                n += 1                              # 다음 칸에 적음
-            line[n][ppl[i * 2]] = [ppl[i * 2 + 1]]
+        line[ppl[i*2]].append(ppl[i*2+1])
+
+    BFS(s)
+    a = max(visited)
+    max_n = 0
+    for i in range(len(line)):
+        if visited[i] == a:             # 가장 마지막에 받은사람 중에 제일 높은 번호
+            max_n = i
+
+    print(f'#{t} {max_n}')
+
 
 
