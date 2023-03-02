53b1246f1984dfd47e4bb156d043c452394644a2
bbangkku <chriskim9512@gmail.com>
2023-03-02 17:52:21 +0900

    study

---
+++ baekjoon/binary_search/2110.py
@@ -0,0 +1,6 @@
+
+## 2110_ 공유기 설치
+
+N, C = map(int, input().split())            # N 집의 개수/ C 공유기 개수
+house = [int(input()) for _ in range(N)]
+house.sort()
--- baekjoon/binary_search/2805.py
+++ baekjoon/binary_search/2805.py
@@ -4,8 +4,20 @@
 N, M = map(int, input().split())        # N: 나무의 수 / M: 가져가려는 나무의 길이
 tree = list(map(int, input().split()))
 start, end = 1, max(tree)
-ans = 0
+
 
 while start <= end:
+    mid = (start + end) // 2
+    get_tr = 0
+    for t in tree:
+        if t - mid > 0:
+            get_tr += (t - mid)
+    if get_tr == M:
+        break
+    elif get_tr > M:
+        start = mid + 1
+    elif get_tr < M:
+        end = mid - 1
 
 
+print(mid)
